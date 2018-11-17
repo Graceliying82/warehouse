0e19330c540641b6b251b292f9654b0721385c2d
Graceliying82 <Gracejustingrace@gmail.com>
2018-11-16 19:36:48 -0500

    Delete key.js

--- wmsServer/config/key.js
+++
@@ -1,7 +0,0 @@
-// add this file to .gitignore
-module.exports = {
-  google : {
-    clientID : '899689734340-pqjem5gb7no3mmgbv35r666fm7m32i97.apps.googleusercontent.com',
-    clientSecret: 'Bqf6keaOPO8pz2rMoE53RjX4'
-  }
-}
