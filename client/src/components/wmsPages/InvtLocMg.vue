7c6c8cdb73aa0fb33d3781fceacb219fa13ce4ca
Graceliying82 <Gracejustingrace@gmail.com>
2018-12-03 13:28:08 -0500

    Working on InvtLocMg

--- client/src/components/wmsPages/InvtLocMg.vue
+++ client/src/components/wmsPages/InvtLocMg.vue
@@ -1,14 +1,39 @@
 <template>
-  <div v-if="$store.state.isUserLoggedIn">
-    <v-content>
-      <v-container fill-height>
-        <v-layout justify-center align-center>
-          <v-flex shrink>
-            <h1>Locations Management Under Construction</h1>
-          </v-flex>
-        </v-layout>
-      </v-container>
-    </v-content>
+  <div  v-if="$store.state.isUserLoggedIn">
+    <v-layout justify-center>
+      <h1>Inventory Location Management</h1>
+    </v-layout>
+    <v-layout  justify-cente>
+      <v-flex ma-5 xs4>
+        <panel title = "Add New Location">
+          <v-text-field
+            v-model="LocationID"
+            label="Location ID"
+            required
+          ></v-text-field>
+          <v-text-field
+            v-model="LocationNickName"
+            label="Location Nick Name"
+          ></v-text-field>
+          <v-btn>Submit</v-btn>
+        </panel>
+      </v-flex>
+    </v-layout>
+    <v-layout>
+      <v-flex ma-5>
+        <panel title = "Current Locations">
+          <v-text-field
+            v-model="LocationID"
+            label="Location ID"
+            required
+          ></v-text-field>
+          <v-text-field
+            v-model="LocationNickName"
+            label="Location Nick Name"
+          ></v-text-field>
+        </panel>
+      </v-flex>
+    </v-layout>
   </div>
 </template>
 
