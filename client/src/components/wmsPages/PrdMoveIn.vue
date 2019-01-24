398a6fe7e30d6c456e6834a2324a28716fb2f897
Graceliying82 <Gracejustingrace@gmail.com>
2019-01-23 19:47:56 -0500

    Move in product edit and delete

--- client/src/components/wmsPages/PrdMoveIn.vue
+++ client/src/components/wmsPages/PrdMoveIn.vue
@@ -20,7 +20,6 @@
             </v-card-title>
             <v-card-text>
               <h3 align='left'>Move In location: {{miLoc1}}</h3>
-              <h3 align='left'>UPC: {{UPC1}}</h3>
               <br>
               <v-layout>
                   <v-flex>
@@ -38,6 +37,16 @@
                         class="text-xs-left">
                         {{ props.item.qty }}
                       </td>
+                      <td :key="it.UPC + '-action'" class="text-xs-left">
+                        <!-- Start of Action buttons -->
+                        <v-btn icon class="mx-0" @click="editItem(props.item)">
+                          <v-icon color="teal">edit</v-icon>
+                        </v-btn>
+                        <v-btn icon class="mx-0" @click="deleteItem(props.item)">
+                          <v-icon color="teal">delete_forever</v-icon>
+                        </v-btn>
+                        <!-- End of Action buttons -->
+                      </td>
                     </template>
                   </v-data-table>
                   </v-flex>
@@ -49,7 +58,37 @@
         </v-flex>
       </v-flex>
     </v-layout>
-     <!-- End of Move In items list -->
+    <!-- End of Move In items list -->
+    <!-- Dialog -->
+    <v-dialog v-model="dialog" max-width="500px">
+      <v-card>
+        <v-card-title>
+          <span class="headline">Edit Items</span>
+        </v-card-title>
+        <v-card-text>
+          <v-container grid-list-md>
+            <v-layout column>
+              <v-flex xs12 sm6 md4>
+                <v-text-field
+                  v-model="editedItem.UPC"
+                  readonly
+                  box
+                  label="UPC"></v-text-field>
+              </v-flex>
+              <v-flex xs12 sm6 md4>
+                <v-text-field v-model="editedItem.qty" label="Quantity"></v-text-field>
+              </v-flex>
+            </v-layout>
+          </v-container>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn color="blue darken-1" flat @click="close">Cancel</v-btn>
+          <v-btn color="blue darken-1" flat @click="save">Save</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+    <!-- End of Dialog -->
   </div>
 </template>
 
@@ -59,6 +98,12 @@ import ProductInv from '@/services/productInv'
 export default {
   data () {
     return {
+      dialog: false,
+      editedIndex: -1,
+      editedItem: {
+        UPC: '',
+        qty: 0
+      },
       rules: {
         qnRule1: val => val < 1000000 || 'Not a valid number',
         qnRule2: val => val >= 0 || 'Not a valid number'
@@ -70,17 +115,24 @@ export default {
       alertType1: 'success',
       showAlert1: false,
       message1: '',
-      miItems1Headers: [{
-        text: 'UPC',
-        align: 'left',
-        value: 'UPC',
-        sortable: false
-      },
-      { text: 'Quantity',
-        align: 'left',
-        sortable: false,
-        value: 'qty'
-      }],
+      miItems1Headers: [
+        {
+          text: 'UPC',
+          align: 'left',
+          value: 'UPC',
+          sortable: false
+        },
+        { text: 'Quantity',
+          align: 'left',
+          sortable: false,
+          value: 'qty'
+        },
+        { text: 'Actions',
+          align: 'left',
+          value: 'UPC',
+          sortable: false
+        }
+      ],
       miItems1: [],
       rowsPerPageItems: [30, 60, { 'text': '$vuetify.dataIterator.rowsPerPageAll', 'value': -1 }]
     }
@@ -105,6 +157,24 @@ export default {
       this.clearAlert()
       this.clearMoveItems()
     },
+    editItem (item) {
+      this.editedIndex = this.miItems1.indexOf(item)
+      this.editedItem = Object.assign({}, item)
+      this.dialog = true
+    },
+    deleteItem (item) {
+      const index = this.miItems1.indexOf(item)
+      confirm('Are you sure you want to delete this item?') && this.miItems1.splice(index, 1)
+    },
+    close () {
+      this.editedItem.UPC = ''
+      this.editedItem.qty = 0
+      this.dialog = false
+    },
+    save () {
+      this.miItems1[this.editedIndex].qty = this.editedItem.qty
+      this.dialog = false
+    },
     handleUPCInput (input) {
       if (input === 'WMS-RECEIVING-SUBMIT') {
         this.submit1()
@@ -121,7 +191,6 @@ export default {
           this.miItems1[idx].qty++
         } else {
           this.miItems1.push({UPC: input, qty: 1})
-          console.log(this.miItems1)
         }
       }
     },
@@ -151,7 +220,6 @@ export default {
     },
     async checkLocationExisted (locID) {
       try {
-        console.log('Here')
         let locIDExisted = await Location.checkLocationExisted(locID)
         if (locIDExisted.data.length === 0) {
           // Location doesn't existed!
