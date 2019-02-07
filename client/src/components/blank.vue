d64033a9f3505f800b2255381e0e6aca13c7245e
Graceliying82 <Gracejustingrace@gmail.com>
2019-02-07 09:58:56 -0500

    Update blank vue file for future programming

--- client/src/components/blank.vue
+++ client/src/components/blank.vue
@@ -1,9 +1,38 @@
 <template>
+  <div v-if="$store.state.isUserLoggedIn">
+    <v-layout>
+      <v-flex xs8>
+          <v-alert
+            v-show = showAlert1
+            :type = alertType1
+            outline>
+              {{message1}}
+            </v-alert>
+        </v-flex>
+    </v-layout>
+  </div>
 </template>
 
 <script>
+// import Shipment from '@/services/Shipment'
 export default {
   data () {
+    return {
+      alertType1: 'success',
+      showAlert1: false,
+      message1: ''
+    }
+  },
+  methods: {
+    clearAlert () {
+      this.showAlert1 = false
+      this.message1 = ''
+    },
+    setAlert (type, message) {
+      this.message1 = message
+      this.alertType1 = type
+      this.showAlert1 = true
+    },
   }
 }
 </script>
